182d1674c53936eb62ccd40ff4a1d8b8a609a9f5
Wylde <76519000+WyldeTuk@users.noreply.github.com>
2022-06-11 04:04:24 -0300

    Update data.c

--- data.c
+++ data.c
@@ -1,6 +1,3 @@
-// Faça um programa em C que leia uma estrutura contendo dia, mês e ano e
-// imprima a data tanto no formato BR (dd/mm/aaaa) quanto no formato US
-// (mm/dd/aaaa).
 #include <stdio.h>
 #include <string.h>
 int main() {
@@ -18,12 +15,21 @@ int main() {
   printf("insira o ano: ");
   scanf("%d", &data.ano);
 
-  if (data.dia >= 1 && data.dia <= 31) {
-    if (data.mes >= 1 && data.mes <= 12) {
-      printf(" Br--%d/%d/%d\n", data.dia, data.mes, data.ano);
-      printf(" Us--%d/%d/%d\n", data.mes, data.dia, data.ano);
-    } else {
-      printf("essa não é uma data valida");
+  if (data.mes >= 1 && data.mes <= 12) {
+    if (data.mes == 2) {
+      if (data.dia >= 1 && data.dia <= 29) {
+        printf(" Br--%d/%d/%d\n", data.dia, data.mes, data.ano);
+        printf(" Us--%d/%d/%d\n", data.mes, data.dia, data.ano);
+      } else {
+        printf("essa não é uma data valida");
+      }
+    } else if (data.mes != 2) {
+      if (data.dia >= 1 && data.dia <= 31) {
+        printf(" Br--%d/%d/%d\n", data.dia, data.mes, data.ano);
+        printf(" Us--%d/%d/%d\n", data.mes, data.dia, data.ano);
+      } else {
+        printf("essa não é uma data valida");
+      }
     }
   } else {
     printf("essa não é uma data valida");
